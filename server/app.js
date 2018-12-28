eeadb3423d9160664a389480f6097d59c226b88a
Richard F <richardfornario@gmail.com>
2018-12-28 17:12:18 -0600

    add mongo

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
+const mongoUser = process.env.DB_USERNAME;
+const mongoPass = process.env.DB_PASSWORD;
+const mongoPath = process.env.DB_PATH;
+const mongoPort = process.env.DB_PORT;
+
+mongoose.connect(`mongodb://${mongoUser}:${mongoPass}@${mongoPath}:${mongoPort}/admin`,{ useNewUrlParser: true });
+var Poll = mongoose.model('Poll', {question: 'string' });
+//var myPoll = new Poll({question: 'What is going on?'});
+//myPoll.save(); 
+
 app.get('/api/', function(req, res) {
-  res.send('Hello World from the server!');
+  Poll.findOne({}, 'question', (err, poll) => {
+    res.send(poll.question);
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
